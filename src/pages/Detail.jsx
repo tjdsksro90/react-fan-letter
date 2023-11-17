334ce5a45ae9d40b46191ccf2325838338c510f3
hojun.yoon <tjdsksro@gmail.com>
2023-11-17 19:13:36 +0900

    props-drilling

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -2,124 +2,25 @@ import React, { useEffect, useState } from "react";
 import { Link, useLocation, useNavigate, useParams } from "react-router-dom";
 import data from "../fakeData.json";
 import styled from "styled-components";
-
-const blue = {
-  basic: "#3747ff",
-  light: "#838cf5",
-  dark: "#1e29ab",
-  back: "#c7c9ff",
-};
-
-const red = {
-  basic: "#e50000",
-  light: "#ff4c4c",
-  dark: "#a70000",
-  back: "#ffbbbb",
-};
-
-const DetailBg = styled.div`
-  background-image: url("/img/background.jpg");
-  background-size: cover;
-  background-position: top center;
-  background-repeat: no-repeat;
-  background-color: ${(props) => props.color.dark};
-  background-blend-mode: multiply;
-  width: 100%;
-`;
-
-const DetailHome = styled.span`
-  position: absolute;
-  top: 0;
-  left: 50%;
-  transform: translateX(-50%);
-  font-size: 50px;
-  font-weight: 700;
-  color: #fff;
-  background: rgb(0 0 0 / 50%);
-  width: 100%;
-  text-align: center;
-  padding: 10px;
-`;
-
-const DetailVh = styled.div`
-  min-height: 100vh;
-`;
-const DetailBox = styled.div`
-  text-align: center;
-  position: relative;
-  top: 150px;
-  width: 420px;
-  max-width: 90%;
-  margin: 0 auto;
-  padding: 30px;
-  background: #dfdfdf;
-`;
-const DetailImg = styled.figure`
-  width: 100px;
-  height: 100px;
-  border-radius: 50%;
-  margin: 0 auto;
-  overflow: hidden;
-  background: #ccc;
-`;
-const DetailImg100 = styled.img`
-  width: 100%;
-`;
-const DetailH2 = styled.h2`
-  font-size: 32px;
-  font-weight: bold;
-  margin-top: 20px;
-  line-height: 1.2;
-  color: ${(props) => props.color.dark};
-`;
-const DetailTime = styled.time`
-  font-size: 14px;
-  margin-top: 20px;
-  display: block;
-`;
-const DetailTo = styled.div`
-  margin-top: 10px;
-  font-style: italic;
-`;
-const DetailContent = styled.div`
-  padding: 20px;
-  background: #ededed;
-  margin-top: 20px;
-  display: flex;
-  textArea {
-    width: 100%;
-    padding: 8px;
-    border: 1px solid #ccc;
-    border-radius: 5px;
-  }
-`;
-const DetailBtnWrap = styled.div`
-  position: absolute;
-  top: 30px;
-  right: 30px;
-  display: flex;
-  flex-direction: column;
-  gap: 5px;
-`;
-const DetailBtn = styled.button`
-  width: 75px;
-  height: 25px;
-  border: 0;
-  border-radius: 5px;
-  cursor: pointer;
-  background-color: ${(props) => props.color.basic};
-  color: #fff;
-  &:hover {
-    background-color: ${(props) => props.color.dark};
-    color: #fff;
-  }
-  &:active {
-    background-color: ${(props) => props.color.light};
-    color: #fff;
-  }
-`;
+import {
+  DetailBg,
+  DetailBox,
+  DetailBtn,
+  DetailBtnWrap,
+  DetailContent,
+  DetailH2,
+  DetailHome,
+  DetailImg,
+  DetailImg100,
+  DetailTime,
+  DetailTo,
+  DetailVh,
+  blue,
+  red,
+} from "assets/BasicStyle";
 
 function Detail() {
+  const navigate = useNavigate();
   const param = useParams();
   // const location = useLocation();
   // const data = location.state;
@@ -149,8 +50,6 @@ function Detail() {
     (item) => item.id === param.id
   );
   console.log(detailIndex);
-
-  const navigate = useNavigate();
 
   const [editCheck, setEditCheck] = useState(false);
 
